4273b37716ec1e5803dae1a170052ed86e672f12
Hnicky <hour.nicky@gmail.com>
2018-03-16 10:10:01 +0100

    calc

---
+++ index.js
@@ -0,0 +1,107 @@
+// Ligne 1
+function sept(){
+  var x = document.getElementById("sept").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function huit(){
+  var x = document.getElementById("huit").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function neuf(){
+  var x = document.getElementById("neuf").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function multiplier(){
+  var x = document.getElementById("fois").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + " " + x + " ";
+}
+
+// Ligne 2
+function quatre(){
+  var x = document.getElementById("quatre").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function cinq(){
+  var x = document.getElementById("cinq").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function six(){
+  var x = document.getElementById("six").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function moins(){
+  var x = document.getElementById("moins").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + " " + x + " ";
+}
+
+// Ligne 3
+function un(){
+  var x = document.getElementById("un").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function deux(){
+  var x = document.getElementById("deux").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function trois(){
+  var x = document.getElementById("trois").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function plus(){
+  var x = document.getElementById("plus").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + " " + x + " ";
+}
+
+// Ligne 4
+function zero(){
+  var x = document.getElementById("zero").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function virgule(){
+  var x = document.getElementById("virgule").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + x;
+}
+
+function diviser(){
+  var x = document.getElementById("diviser").value;
+  var valeur = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = valeur + " " + x + " ";
+}
+
+function egal(){
+	var valeur = document.getElementById("resultat").value;
+	var result = eval(valeur);
+	document.getElementById("resultat").value = result;
+}
+
+
+
+
+
+
+
+//ok
